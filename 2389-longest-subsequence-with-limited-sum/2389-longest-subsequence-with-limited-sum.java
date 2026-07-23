d19cb655ecdd2c1d0147602d14d3749097ef8ab5
Pruthviraj Gavhane <97662679+Pruthviraj141@users.noreply.github.com>
2026-07-23 20:31:12 +0530

    Time: 23 ms (6.05%), Space: 46.8 MB (85.39%) - LeetHub

---
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] answerQueries(int[] nums, int[] q) {
+
+        Arrays.sort(nums);
+
+        int[] ans = new int[q.length];
+
+        int[] n = new int[nums.length];
+        int sum = 0;
+
+        for(int i = 0; i<nums.length; i++){
+            sum =  sum + nums[i];
+            n[i] = sum;
+        }
+
+        int c = 0;
+
+
+        for(int i = 0; i<q.length; i++){
+            int count=0;
+            for(int j = 0; j<nums.length; j++){
+                if(q[i] >= n[j]){
+                    count++;
+                }
+            }
+            ans[c++] = count;
+        }
+
+        return ans;
+    }
+}
