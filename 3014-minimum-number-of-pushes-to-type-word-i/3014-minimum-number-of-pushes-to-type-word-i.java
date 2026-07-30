994585547ca19503a89334877dd0b6c52c19c555
Pruthviraj Gavhane <97662679+Pruthviraj141@users.noreply.github.com>
2026-07-30 20:12:03 +0530

    0 ms (100%) 43.3 MB (28.23%) 07-30-2026 Easy .java Math 3014-minimum-number-of-pushes-to-type-word-i

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,11 +1,11 @@
 class Solution {
     public int minimumPushes(String word) {
-        int res = 0;
-
-        for(int i = 0 ; i < word.length(); i ++){
-            res += (i/8) + 1;
-
-        }
-        return res;
+        int n = word.length();
+        
+        int groups = n / 8; // Number of fully filled layers (of 8 keys)
+        int rem = n % 8;    // Remaining letters on the next layer
+        
+        // Math formula replacing the entire loop
+        return (4 * groups * (groups + 1)) + (rem * (groups + 1));
     }
 }
