d7e9d719b39dbdc97ca703e32d73fe740892d04a
Pruthviraj Gavhane <97662679+Pruthviraj141@users.noreply.github.com>
2026-08-08 19:11:57 +0530

    7 ms (58.1%) 47.2 MB (13.54%) 08-08-2026 Easy .java Hash Table 0387-first-unique-character-in-a-string

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        
+        int[] ans = new int[26];
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+
+            ans[ch - 'a']++;
+
+        }
+
+        for(int i = 0; i<s.length(); i++){
+            if(ans[s.charAt(i) - 'a'] == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
