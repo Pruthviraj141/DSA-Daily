5280c7764f918cf152624b3f194041511b1396f5
Pruthviraj Gavhane <97662679+Pruthviraj141@users.noreply.github.com>
2026-08-31 16:13:54 +0530

    0 ms (100%) 43 MB (8.96%) 08-31-2026 Easy .java String 1221-split-a-string-in-balanced-strings

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int bal = 0;
+        int cnt = 0;
+         
+         for(char c: s.toCharArray()){
+            if(c =='R'){
+                bal++;
+            }else{
+                bal--;
+            }
+
+            if(bal ==0){
+                cnt++;
+            }
+         }
+            return cnt;
+
+
+    }
+}
