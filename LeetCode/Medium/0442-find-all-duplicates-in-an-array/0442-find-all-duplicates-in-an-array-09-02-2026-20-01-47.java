27b0014f4a031520c8ca068b9d4d75edc5354fad
Pruthviraj Gavhane <97662679+Pruthviraj141@users.noreply.github.com>
2026-09-02 20:01:48 +0530

    22 ms (33.36%) 72.9 MB (5.78%) 09-02-2026 Medium .java Array 0442-find-all-duplicates-in-an-array

---
+++ LeetCode/Medium/0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array-09-02-2026-20-01-47.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+
+HashSet<Integer> set = new HashSet<>();
+ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < nums.length; i++){
+
+            if(set.contains(nums[i])){
+                list.add(nums[i]);
+            }
+            set.add(nums[i]);
+        }
+        return list;
+        
+    }
+}
